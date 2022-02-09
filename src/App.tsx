8fc029229569a1216492638a542fa4c9bff6f548
Simon VDB <simonvdbroeck@gmail.com>
2022-02-09 16:25:05 +0100

    add plugin demo

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./index.css";
+import "./App.css";
 
-function App() {
+import { SkedifyPlugin, SkedifyPluginProvider } from "@skedify/plugin-react";
+
+const config = {
+  appointment: {
+    subject_id: "21"
+  }
+};
+
+const scriptSrc = `https://plugin.staging.skedify.io/bankvanbreda/skedify-plugin.js`;
+
+const defaultConfig = {
+  scriptSrc,
+  language: "nl-be",
+  flow: "S'TQC",
+  meeting_types: "video",
+  timezone: "Europe/Brussels",
+  oauth: {
+    client: "3thwHbBTXFmnfQIN2R0zug0UbKiP7DTallmDJqWG"
+  },
+  appointment: {
+    office_id: "1"
+  }
+};
+
+export default function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <SkedifyPluginProvider value={defaultConfig}>
+      <div className="App">
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+
+        <div style={{ display: "flex", gap: "4rem", justifyItems: "center" }}>
+          <SkedifyPlugin id="skedify-plugin-wrapper" style={{ width: 400 }} configuration={config} />
+          <SkedifyPlugin
+            id="skedify-plugin-wrapper"
+            style={{ width: 400 }}
+            configuration={{ appointment: { subject_id: "20" } }}
+          />
+          <SkedifyPlugin
+            id="skedify-plugin-wrapper"
+            style={{ width: 400 }}
+            configuration={{ appointment: { subject_id: "9" } }}
+          />
+        </div>
+      </div>
+    </SkedifyPluginProvider>
   );
 }
-
-export default App;
